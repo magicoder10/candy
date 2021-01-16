a58b98f0807a49845f85b1c8d9be6e00e1d95807
Harry Liu <magicoder20@gmail.com>
2021-01-15 22:18:48 -0800

    Fix broken game

---
+++ game/player/character.go
@@ -0,0 +1,46 @@
+package player
+
+type character struct {
+	walkCycleOffset walkCycleOffset
+}
+
+var BlackBoy = character{
+	walkCycleOffset: walkCycleOffset{
+		x: 0,
+		y: spriteColHeight,
+	}}
+var BlackGirl = character{
+	walkCycleOffset: walkCycleOffset{
+		x: 0,
+		y: 0,
+	}}
+var BrownBoy = character{
+	walkCycleOffset: walkCycleOffset{
+		x: spriteRowWidth,
+		y: spriteColHeight,
+	}}
+var BrownGirl = character{
+	walkCycleOffset: walkCycleOffset{
+		x: spriteRowWidth,
+		y: 0,
+	}}
+var YellowBoy = character{
+	walkCycleOffset: walkCycleOffset{
+		x: spriteRowWidth * 2,
+		y: spriteColHeight,
+	}}
+var YellowGirl = character{
+	walkCycleOffset: walkCycleOffset{
+		x: spriteRowWidth * 2,
+		y: 0,
+	}}
+var OrangeBoy = character{
+	walkCycleOffset: walkCycleOffset{
+		x: spriteRowWidth * 3,
+		y: spriteColHeight,
+	}}
+var OrangeGirl = character{
+	walkCycleOffset: walkCycleOffset{
+		x: spriteRowWidth * 3,
+		y: 0,
+	}}
