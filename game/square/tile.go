85674df589b7ab8405314ea9ce0e5ef952e139a0
Harry Liu <magicoder20@gmail.com>
2021-01-16 10:52:36 -0800

    Clean up tile state

--- game/square/tile.go
+++ game/square/tile.go
@@ -14,30 +14,23 @@ const brokenTileYOffset = 2
 var _ Square = (*Tile)(nil)
 
 type Tile struct {
-	imageXOffset int
-	imageYOffset int
-	xOffset      int
-	yOffset      int
-	showItem     bool
-	isBroken     bool
-	canEnter     bool
-	gameItem     gameitem.GameItem
+	state tileState
 }
 
-func (t *Tile) ShouldRemove() bool {
-	return t.canEnter && t.gameItem == gameitem.None
+func (t Tile) ShouldRemove() bool {
+	return t.state.shouldRemove()
 }
 
 func (t *Tile) UnblockFire() {
-	t.canEnter = true
+	t.state = t.state.unblockFire()
 }
 
-func (t *Tile) IsBlocking() bool {
+func (t Tile) IsBlocking() bool {
 	return true
 }
 
 func (t *Tile) Break() {
-	t.isBroken = true
+	t.state = t.state.breakTile()
 }
 
 func (t Tile) IsBreakable() bool {
@@ -45,69 +38,50 @@ func (t Tile) IsBreakable() bool {
 }
 
 func (t Tile) Draw(batch graphics.Batch, x int, y int) {
-	bound := graphics.Bound{
-		X:      t.imageXOffset,
-		Y:      t.imageYOffset,
-		Width:  64,
-		Height: 80,
-	}
-
-	newX := x + t.xOffset
-	newY := y + t.yOffset
-	if !t.isBroken {
-		batch.DrawSprite(newX, newY, y, bound, 1)
-	}
-
-	if t.gameItem != gameitem.None {
-		if t.isBroken {
-			batch.DrawSprite(
-				newX+brokenTileXOffset, newY+brokenTileYOffset, y,
-				t.gameItem.GetBound(), 1)
-		} else if t.showItem {
-			batch.DrawSprite(
-				newX+revealItemXOffset, newY+revealItemYOffset, y+revealItemZOffset,
-				t.gameItem.GetBound(), 0.6)
-		}
-	}
+	t.state.draw(batch, x, y)
 }
 
 func (t Tile) CanEnter() bool {
-	return t.canEnter
+	return t.state.canEnter()
 }
 
-func (t *Tile) RevealItem() gameitem.GameItem {
-	t.showItem = true
-	return t.gameItem
+func (t *Tile) RevealItem() {
+	t.state.revealItem()
 }
 
-func (t *Tile) HideItem() gameitem.GameItem {
-	t.showItem = false
-	return t.gameItem
+func (t *Tile) HideItem() {
+	t.state.hideItem()
 }
 
 func (t *Tile) RemoveItem() gameitem.GameItem {
-	gameItem := t.gameItem
-	t.gameItem = gameitem.None
-	return gameItem
+	return t.state.removeItem()
 }
 
 func newYellow(gameItem gameitem.GameItem) Tile {
 	return Tile{
-		imageXOffset: 576,
-		imageYOffset: 304,
-		xOffset:      -4,
-		yOffset:      -2,
-		gameItem:     gameItem,
+		state: &tileSolidState{
+			tileSharedState{
+				imageXOffset: 576,
+				imageYOffset: 304,
+				xOffset:      -4,
+				yOffset:      -2,
+				gameItem:     gameItem,
+			},
+		},
 	}
 }
 
 func newGreen(gameItem gameitem.GameItem) Tile {
 	return Tile{
-		imageXOffset: 576,
-		imageYOffset: 224,
-		xOffset:      -4,
-		yOffset:      -2,
-		gameItem:     gameItem,
+		state: &tileSolidState{
+			tileSharedState{
+				imageXOffset: 576,
+				imageYOffset: 224,
+				xOffset:      -4,
+				yOffset:      -2,
+				gameItem:     gameItem,
+			},
+		},
 	}
 }
 
