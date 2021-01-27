a1d8c10c2ca23ecce75c93ea9ee75f771aa65dfc
Harry Liu <magicoder20@gmail.com>
2021-01-26 21:47:33 -0800

    Add env

--- game_server.go
+++ game_server.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"candy/env"
 	"candy/observability"
 	"candy/pubsub"
 	"candy/server"
 )
 
 func main() {
+	type pubSubConfig struct {
+		PubSubServerHost string `env:"PUBSUB_SERVER_HOST" default:"localhost"`
+		PubSubServerPort int    `env:"PUBSUB_SERVER_PORT" default:"8000"`
+	}
+
+	config := pubSubConfig{}
+	err := env.ParseConfigFromEnv(&config)
+	if err != nil {
+		panic(err)
+	}
+
 	logger := observability.NewLogger(observability.Info)
 
 	pubSubRemote := pubsub.NewRemote(&logger)
@@ -14,11 +26,11 @@ func main() {
 	gameServer := server.NewGame(&logger, pubSubRemote)
 
 	go func() {
-		server.WaitReady("localhost", 8081)
-		pubSubRemote.Start("localhost", 8081)
+		server.WaitReady(config.PubSubServerHost, config.PubSubServerPort)
+		pubSubRemote.Start(config.PubSubServerHost, config.PubSubServerPort)
 	}()
 
-	err := gameServer.Start(8082)
+	err = gameServer.Start(8082)
 	if err != nil {
 		panic(err)
 	}
