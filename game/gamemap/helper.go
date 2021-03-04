626c3d8811da7265f270abf9496a22095d8e9d04
yijiacc <76569613+yijia-cc@users.noreply.github.com>
2021-03-03 19:28:20 -0800

    Update game/gamemap/helper.go
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

--- game/gamemap/helper.go
+++ game/gamemap/helper.go
@@ -7,7 +7,7 @@ import (
 
 func (m Map) GetObjectCell(objectX int, objectY int, objectWidth int, objectHeight int) cell.Cell {
 	return cell.GetCellLocatedAt(
-		objectX-m.GetGridXOffset(), objectY-m.GetGridYOffset(), objectWidth, objectHeight,
+		objectX-m.gridXOffset, objectY-m.gridYOffset, objectWidth, objectHeight,
 		square.Width, square.Width,
 	)
 }
