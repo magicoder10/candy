ab33a10e09982b51548ab87a21e68f723fcf1be4
gloriaswj <gloriaswj@users.noreply.github.com>
2021-04-03 01:58:25 -0700

    Clean up test

--- game/gamemap/dropcandy_test.go
+++ game/gamemap/dropcandy_test.go
@@ -1,56 +1,49 @@
 package gamemap
 
 import (
-	"candy/game/square"
 	"testing"
+
+	"candy/game/square"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDropCandyChecker_CanDropCandy(t *testing.T) {
-	gamemap := &Map{
-		screenX:     3,
-		screenY:     3,
-		batch:       nil,
-		maxRow:      4,
-		maxCol:      4,
-		gridXOffset: 3,
-		gridYOffset: 3,
-		grid: &[][]square.Square{
-			{nil, nil, nil, nil},
-			{nil, nil, nil, nil},
-			{nil, nil, nil, nil},
-			{nil, nil, nil, nil},
-		},
-	}
-	dropCandyChecker := NewDropCandyChecker(gamemap)
-
 	testCases := []struct {
-		name         string
-		playerX      int
-		playerY      int
-		playerWidth  int
-		playerHeight int
-		threshold    bool
+		name                 string
+		gameMap              *Map
+		playerX              int
+		playerY              int
+		expectedCanDropCandy bool
 	}{
-		{name: "testcase1",
-			playerX:      1,
-			playerY:      2,
-			playerWidth:  1,
-			playerHeight: 1,
-			threshold:    true,
+		{
+			name: "map empty",
+			gameMap: &Map{
+				batch:  nil,
+				maxRow: 4,
+				maxCol: 4,
+				grid: &[][]square.Square{
+					{nil, nil, nil, nil},
+					{nil, nil, nil, nil},
+					{nil, nil, nil, nil},
+					{nil, nil, nil, nil},
+				},
+			},
+			playerX:              1,
+			playerY:              2,
+			expectedCanDropCandy: true,
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			canDropCandyChecker :=
-				dropCandyChecker.CanDropCandy(
-					testCase.playerX,
-					testCase.playerY,
-					testCase.playerWidth,
-					testCase.playerHeight)
-			assert.Equal(t, testCase.threshold, canDropCandyChecker)
+			dropCandyChecker := NewDropCandyChecker(testCase.gameMap)
+			gotCanDropCandyCheck := dropCandyChecker.CanDropCandy(
+				testCase.playerX,
+				testCase.playerY,
+				1,
+				1)
+			assert.Equal(t, testCase.expectedCanDropCandy, gotCanDropCandyCheck)
 		})
 	}
 
